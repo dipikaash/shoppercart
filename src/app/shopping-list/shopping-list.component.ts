d5cba62a758177a7e36c4ec430b3eef5ef61d57f
Your Name <you@example.com>
2020-07-18 22:17:41 +0530

    initial template structure

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Ingredient } from '../shared/ingredient.model';
+import { from } from 'rxjs';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Oranges', 10),
+    new Ingredient('grapes', 50),
+  ];
   constructor() { }
 
   ngOnInit(): void {
